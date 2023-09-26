dd341b6c010e8ce4be6dadc3636f14402414e42c
SUMANTHTPs <sumanthtps@gmail.com>
2023-09-26 11:17:42 +0530

    Updating logo

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,16 +1,23 @@
+"use client";
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Logo = () => {
+  const router = useRouter();
+  const navigateToHome = () => {
+    router.push("/")
+  }
   return (
-    <div className='flex justify-center items-center gap-2'>
+    <div className='flex justify-center items-center gap-2 cursor-pointer' onClick={navigateToHome}>
       <Image
         src="/images/shirt.png"
         width={40}
         height={40}
         alt='logo'
       />
-      <h1 style={{ fontFamily: "cursive" }} className='text-red-500 text-xl'>
+      <h1 style={{ fontFamily: "cursive" }} className='text-blue-900 font-bold text-3xl'>
         Trendy Wear
       </h1>
     </div>
