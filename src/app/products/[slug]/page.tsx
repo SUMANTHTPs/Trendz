b4649c26121f3cac7638b5d06f4ddcaf8c8f59d8
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-01 19:41:36 +0530

    Fix

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -84,10 +84,10 @@ export default function Product({ params }: ProductProps) {
                         </div>
                         <div className="flex justify-between">
                             <span className="title-font font-medium text-2xl text-gray-900">₹{product?.price} /-</span>
-                            <div className="flex gap-2">
+                            <div className="flex gap-3">
                                 <button className="flex  text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded" onClick={() => dispatch(addToCart(productId))}>Add to cart</button>
                                 <button className="flex  text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded">Buy now</button>
-                                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500">
                                     <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                                     </svg>
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,13 +1,13 @@
-import { CartItemProps } from "@/redux/features/cartSlice";
-
 export function setCartToLocalStorage(item: any) {
   return localStorage.setItem("cart", JSON.stringify(item));
 }
 
 export function getCartFromLocalStorage() {
-  const storedItems = localStorage.getItem("cart");
-  const cartItems = storedItems ? JSON.parse(storedItems) : [];
-  return cartItems;
+  if (typeof window !== "undefined") {
+    const storedItems = localStorage.getItem("cart");
+    return storedItems ? JSON.parse(storedItems) : [];
+  }
+  return [];
 }
 
 export function removeCartFromLocalStorage() {
