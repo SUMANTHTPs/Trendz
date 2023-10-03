cd5e66e70a2dcf48da102055688dbdae7c7164ca
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-04 01:28:21 +0530

    models

---
+++ src/models/Product.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    img: { type: String, required: true },
+    category: { type: String, required: true },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+    availableQuantity: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+export default model("Product", ProductSchema);
---
+++ src/models/User.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    
+  },
+  { timestamps: true }
+);
+
+export default model("User", UserSchema);
