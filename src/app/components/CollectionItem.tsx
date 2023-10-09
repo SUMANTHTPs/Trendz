b3f6ae65d891a042ee5721c9052b4b058bfae55f
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-09 21:20:28 +0530

    col change

--- src/app/components/CollectionItem.tsx
+++ src/app/components/CollectionItem.tsx
@@ -6,13 +6,17 @@ export type CollectionProps = {
     link: string
     text: string
 }
-function CollectionItem(item: CollectionProps) {
-    const { img, link, text } = item;
+
+function CollectionItem({ img, link, text }: CollectionProps) {
     return (
-        <div className='flex flex-wrap gap-3 item-center justify-center w-[40vw] h-[60vh]'>
+        <div className='flex flex-col items-center justify-center w-full md:w-[45vw] rounded-2xl'>
             <Link href={link}>
-                <img className='w-[50vw]' src={img} alt={`${text}-collection-image`} />
-                <p>{text}</p>
+                <div className="relative">
+                    <img style={{height: "500px"}} className='w-[100vw]' src={img} alt={`${text}-collection-image`} />
+                    <p className="absolute bottom-0 left-0 right-0 text-center bg-gray-400 text-white p-2">
+                        {text}
+                    </p>
+                </div>
             </Link>
         </div>
     )
--- src/app/components/Collections.tsx
+++ src/app/components/Collections.tsx
@@ -8,7 +8,7 @@ function Collections() {
       <h1 className="text-2xl text-gray-800 uppercase font-bold text-center">
         Collections
       </h1>
-      <div className="flex w-[60vw]">
+      <div className="flex flex-wrap gap-10 items-center justify-center mt-3">
         {collectionItems.map((item: CollectionProps) => (
           <CollectionItem key={item.text} {...item} />
         ))}
