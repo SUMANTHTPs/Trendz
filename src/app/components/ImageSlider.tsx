391b747fe0c1f739d16a21ef8a7bf9ab6bd10395
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-09 13:57:27 +0530

    corrosal complete

--- src/app/components/ImageSlider.tsx
+++ src/app/components/ImageSlider.tsx
@@ -18,13 +18,39 @@ function ImageSlider({ imageUrls }: ImageSliderProps) {
     }
     return (
         <div className='w-full h-full relative'>
-            <Image className='object-cover w-full h-full block' src={imageUrls[imgIndex]} width={1000} height={1000} alt='banner images' />
+            <div style={{ width: "100%", height: "100%", display: "flex", overflow: "hidden" }}>
+                {imageUrls.map(url => (
+                    <Image key={url.blurDataURL} style={{
+                        translate: `${-100 * imgIndex}%`
+                    }}
+                        className='object-cover w-full h-full block flex-shrink-0 flex-grow-0 banner-img'
+                        src={url}
+                        width={1000}
+                        height={1000}
+                        alt='banner images'
+                    />
+                ))}
+            </div>
             <button className='img-slider-button' onClick={handlePrev}>
-                <AiOutlineLeft fill='white'stroke='black' />
+                <AiOutlineLeft className='banner-icons' fill='white' stroke='black' />
             </button>
-            <button className='img-slider-button right-0' style={{right: "0"}} onClick={handleNext}>
-                <AiOutlineRight fill='white' stroke='black' />
+            <button className='img-slider-button right-0' style={{ right: "0" }} onClick={handleNext}>
+                <AiOutlineRight className='banner-icons' fill='white' stroke='black' />
             </button>
+            <div
+                style={{
+                    position: "absolute",
+                    bottom: "0.5rem",
+                    left: "50%",
+                    translate: "-50%",
+                    display: "flex",
+                    gap: "0.25rem"
+                }}
+            >
+                {imageUrls.map((_, index) => (
+                    <button key={index} className={`flex w-2 h-2 bg-white rounded  ${index === imgIndex && "bg-gray-600 border border-white scale-105"}`} onClick={() => setImgIndex(index)}></button>
+                ))}
+            </div>
         </div>
     )
 }
