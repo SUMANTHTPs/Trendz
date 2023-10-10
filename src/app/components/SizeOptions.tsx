1c90f3011d0fac194436a629b3a954f3118e4eb7
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-10 21:45:01 +0530

    Revert

--- src/app/components/SizeOptions.tsx
+++ src/app/components/SizeOptions.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const buttonStyle = "border-2 border-gray-300 ml-1 w-6 h-6 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 p-2 px-4 flex items-center justify-center";
-
 function SizeOptions({ product }: any) {
     const availableSizes = ["S", "M", "L", "XL"];
 
@@ -9,7 +7,7 @@ function SizeOptions({ product }: any) {
         <div className="flex gap-1">
             {availableSizes.map((size) => (
                 product.size.includes(size) && (
-                    <button key={size} value={size} className={buttonStyle}>{size}</button>
+                    <button key={size} value={size} className="border-2 border-gray-300 ml-1 w-6 h-6 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 p-2 px-4 flex items-center justify-center">{size}</button>
                 )
             ))}
         </div>
