60ed26d52e1b05852e04f2a7a50bc93f53ffe233
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-10 21:38:54 +0530

    Size rendering in order

--- src/app/components/SizeOptions.tsx
+++ src/app/components/SizeOptions.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+const buttonStyle = "border-2 border-gray-300 ml-1 w-6 h-6 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 p-2 px-4 flex items-center justify-center";
 
 function SizeOptions({ product }: any) {
+    const availableSizes = ["S", "M", "L", "XL"];
+
     return (
         <div className="flex gap-1">
-            {product.size.map((size: string) => (
-                <button
-                    key={size}
-                    className="border-2 border-gray-300 ml-1 w-6 h-6 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 p-2 px-4 flex items-center justify-center"
-                >
-                    {size}
-                </button>
+            {availableSizes.map((size) => (
+                product.size.includes(size) && (
+                    <button key={size} className={buttonStyle}>{size}</button>
+                )
             ))}
         </div>
-    )
+    );
 }
 
-export default SizeOptions
+export default SizeOptions;
