941193cb725ae92765dc5ce6aa9f25845bf31b53
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-13 22:31:07 +0530

    highlight color and size

--- src/app/components/ColorOptions.tsx
+++ src/app/components/ColorOptions.tsx
@@ -1,22 +1,27 @@
+"use client"
 import { setColor } from '@/redux/features/productSlice';
-import { AppDispatch } from '@/redux/store';
+import { AppDispatch, useAppSelector } from '@/redux/store';
 import React from 'react'
 import { useDispatch } from 'react-redux';
 
-function ColorOptions({product}: any) {
+function ColorOptions({ product }: any) {
     const dispatch = useDispatch<AppDispatch>();
-  return (
-      <div className="flex gap-1">
-          {product.color.map((color: string) => (
-              <button
-                  key={color}
-                  style={{ backgroundColor: color }}
-                  className="border-2 border-gray-300 ml-1 rounded-full w-6 h-6 focus:outline-none"
-                  onClick={() => dispatch(setColor(color))}
-              ></button>
-          ))}
-      </div>
-  )
+    const { color: selectedColor }: any = useAppSelector(store => store.product)
+    return (
+        <div className="flex gap-1">
+            {product.color.map((color: string) => (
+                <button
+                    key={color}
+                    style={{
+                        backgroundColor: color,
+                        border: color.toLowerCase() === selectedColor?.toLowerCase() ? "2px solid #007bff" : ""
+                    }}
+                    className="border-2 border-gray-300 ml-1 rounded-full w-6 h-6 focus:outline-none"
+                    onClick={() => dispatch(setColor(color))}
+                ></button>
+            ))}
+        </div>
+    )
 }
 
 export default ColorOptions
--- src/app/components/SizeOptions.tsx
+++ src/app/components/SizeOptions.tsx
@@ -1,17 +1,28 @@
 import { setSize } from '@/redux/features/productSlice';
-import { AppDispatch } from '@/redux/store';
+import { AppDispatch, useAppSelector } from '@/redux/store';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
 function SizeOptions({ product }: any) {
     const dispatch = useDispatch<AppDispatch>();
     const availableSizes = ["S", "M", "L", "XL"];
+    const { size: selectedSize }: any = useAppSelector(store => store.product)
 
     return (
         <div className="flex gap-1">
             {availableSizes.map((size) => (
                 product.size.includes(size) && (
-                    <button key={size} value={size} onClick={() => dispatch(setSize(size))} className="border-2 border-gray-300 ml-1 w-6 h-6 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 p-2 px-4 flex items-center justify-center">{size}</button>
+                    <button
+                        key={size}
+                        value={size}
+                        onClick={() => dispatch(setSize(size))}
+                        style={size.toLowerCase() === selectedSize?.toLowerCase() ? {
+                            border: "2px solid black",
+                            backgroundColor: 'lightgray'
+                        } : {}}
+                        className={`border-2 border-gray-300 ml-1 w-6 h-6 p-2 px-4 flex items-center justify-center rounded-full`}>
+                        {size}
+                    </button>
                 )
             ))}
         </div>
