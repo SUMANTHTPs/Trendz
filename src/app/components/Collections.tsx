95ad4d32622dff033d6ff91bae4329b561076c76
SUMANTHTPs <sumanthtps@gmail.com>
2023-10-15 00:20:35 +0530

    styles

--- src/app/components/Collections.tsx
+++ src/app/components/Collections.tsx
@@ -8,7 +8,7 @@ function Collections() {
       <h1 className="m-3 p-3 text-4xl text-gray-800 uppercase font-bold text-center">
         Collections
       </h1>
-      <div className="flex flex-wrap gap-10 items-center justify-center mt-3">
+      <div className="flex flex-wrap gap-10 items-center justify-center mt-3 p-3">
         {collectionItems.map((item: CollectionProps) => (
           <CollectionItem key={item.text} {...item} />
         ))}
